9c8ed904e15761afa70f1f6a8cbb7599b340872c
Matthieu Keller <keller.mdpa@gmail.com>
2015-02-11 20:34:20 +0100

    Modified code for resolve problem 54

--- 54/54.py
+++ 54/54.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Author : Matthieu Keller <keller.mdpa@gmail.com>
-
-""" BE note
-"""
 import random
 
 
 class Card(object):
     """ Dictionnaire couleur->no
     """
-    color_code = {'pique' : 0, 'coeur' : 1, 'trefle' : 2, 'carreau' : 3}
-    value_code = {"as" :12, "roi": 11, "dame": 10, "valet":9, "dix": 8,\
-        "neuf": 7, "huit":6, "sept":5, "six":4, "cinq":3, "quatre":2,\
-        "trois": 1, "deux": 0}
+    color_code = {'C': 0, 'D': 1, 'S': 2, 'H': 3}
+    value_code = {"A": 12, "K": 11, "Q": 10, "J": 9, "T": 8,
+                  "9": 7, "8": 6, "7": 5, "6": 4, "5": 3,
+                  "4": 2, "3": 1, "2": 0}
+
     def __init__(self, value, color):
         """ Init a card with his value and his color
         """
@@ -34,7 +31,7 @@ class Card(object):
         else:
             return False
 
-    def __gt__ (self, card):
+    def __gt__(self, card):
         """ overload greater than operator
             """
         if self.value > card.value:
@@ -42,7 +39,7 @@ class Card(object):
         else:
             return False
 
-    def __lt__ (self,card):
+    def __lt__(self, card):
         """ overload lesser than operator
         """
         if self.value < card.value:
@@ -50,20 +47,22 @@ class Card(object):
         else:
             return False
 
-    def __str__ (self):
+    def __str__(self):
         """ overload toString operator
         """
-        return "color :"+str(self.color) + "value"+str(self.value)
+        return "color: "+str(self.color) + " value: "+str(self.value)
+
 
 class Poker(object):
     def gen_fifths(self):
         """ generate fifths list in order to test isQuinte
         """
         fifth = [-1]*9
-        lst=sorted(Card.value_code.items(), lambda x,y: (x[1]-y[1]))
+        lst = sorted(Card.value_code.items(), lambda x, y: (x[1]-y[1]))
         for i in range(9):
             fifth[i] = set([lst[i+j][0] for j in range(5)])
         return fifth
+
     def gen_game(self):
         """ generate all cards of a poker game (52 Cards)
         """
@@ -72,6 +71,7 @@ class Poker(object):
             for value in Card.value_code.keys():
                 game.add(Card(value, color))
         return game
+
     def gen_color(self):
         """ generate color list in order to test isColor
         """
@@ -79,12 +79,12 @@ class Poker(object):
         for couleur in Card.color_code.keys():
             oneColor = set()
             for value in Card.value_code.keys():
-               oneColor.add(Card(value, couleur))
+                oneColor.add(Card(value, couleur))
             color[Card.color_code[couleur]] = oneColor
         return color
+
     def gen_value(self):
-        """ generate value list in order to test isSquare
-        """
+        """ generate value list """
         value = [-1]*13
         for valeur in Card.value_code.keys():
             oneValue = set()
@@ -102,7 +102,7 @@ class Poker(object):
             deck.add(card)
         return deck
 
-    def isColor (self, deck):
+    def isColor(self, deck):
         """ test if the deck is a Flush
         """
         colors = self.gen_color()
@@ -114,7 +114,9 @@ class Poker(object):
     def isQuinte(self, deck):
         """ test if the deck is a Straight
         """
-        code_table = {12: "as", 11 : "roi", 10 : "dame", 9 : "valet", 8 : "dix", 7 : "neuf", 6 : "huit", 5 : "sept", 4 : "six", 3 : "cinq", 2 : "quatre", 1 : "trois", 0 :"deux"}
+        code_table = {12: "A", 11: "K", 10: "Q", 9: "J", 8: "T",
+                      7: "9", 6: "8", 5: "7", 4: "6", 3: "5",
+                      2: "4", 1: "3", 0: "2"}
         fifths = self.gen_fifths()
         plop = set()
         for card in deck:
@@ -130,56 +132,59 @@ class Poker(object):
         """
         return self.isQuinte(deck) and self.isColor(deck)
 
-    def isSquare (self, deck):
+    def isSquare(self, deck):
         """ test if the deck is a Four of a Kind
         """
         values = self.gen_value()
-        tmp = set()
         for value in values:
             if len(deck.intersection(value)) == 4:
                 return True
         return False
 
-    def isThree (self, deck):
+    def isThree(self, deck):
         """ test if the deck is a Three of a Kind
         """
         values = self.gen_value()
         for value in values:
-            if len(deck.intersection(value))==3:
+            if len(deck.intersection(value)) == 3:
                 return True
         return False
 
-    def isOnePair (self, deck):
-        """ test if the deck is a One Pair
-        """
+    def isOnePair(self, deck):
+        """ test if the deck is a One Pair """
         values = self.gen_value()
         for value in values:
             if len(deck.intersection(value)) == 2:
                 return True
         return False
 
-    def isTwoPair (self, deck):
-        """ test if the deck is a Two Pair
-        """
+    def isTwoPair(self, deck):
+        """ test if the deck is a Two Pair """
         c = 0
         values = self.gen_value()
         for value in values:
             if len(deck.intersection(value)) == 2:
-                c+=1
-        return c==2
+                c += 1
+        return c == 2
 
-    def isFull (self, deck):
-        """ test if the decj is a Full House
-        """
+    def isFull(self, deck):
+        """ test if the deck is a Full House """
         return self.isOnePair(deck) and self.isThree(deck)
 
-    def isCombinaison (self, deck):
-        """ test if the deck is a combinaison
-        """
-        if self.isColor(deck) or self.isQuinte(deck) or self.isSquare(deck) or self.isOnePair(deck) or self.isThree(deck):
+    def isCombinaison(self, deck):
+        """ test if the deck is a combination """
+        if self.isColor(deck) or self.isQuinte(deck) or self.isSquare(deck)\
+                or self.isOnePair(deck) or self.isThree(deck):
             return True
         else:
             return False
+
+    def searchHighCard(self, deck):
+        tmp = -1
+        for card in deck:
+            if card.value > tmp:
+                tmp = card.value
+        return tmp
 
     def wichHand(self, deck):
         """ return :
@@ -191,8 +196,7 @@ class Poker(object):
             3 for a Three of Kind
             2 for a Two Pair
             1 for a Pair
-            else 0
-        """
+            else 0 """
         if self.isQuinteFlush(deck):
             return 8
         if self.isSquare(deck):
@@ -211,31 +215,150 @@ class Poker(object):
             return 1
         return 0
 
+    def getPairValue(self, deck):
+        values = self.gen_value()
+        tmp = ''
+        for value in values:
+            if len(deck.intersection(value)) == 2:
+                for v in value:
+                    tmp = v
+                    break
+        return tmp.value
+
+    def getThreeValue(self, deck):
+        values = self.gen_value()
+        tmp = ''
+        for value in values:
+            if len(deck.intersection(value)) == 3:
+                for v in value:
+                    tmp = v
+                    break
+        return tmp.value
+
+    def getSquareValue(self, deck):
+        tmp = 1
+        values = self.gen_value()
+        for value in values:
+            if len(deck.intersection(value)) == 4:
+                for v in value:
+                    tmp = v
+                    break
+        return tmp.value
+
+    def isBetterPair(self, deck1, deck2):
+        if self.getPairValue(deck1) == self.getPairValue(deck2):
+            v = self.getPairValue(deck1)
+
+            # remove pair from deck1
+            to_remove = []
+            for c in deck1:
+                if c.value == v:
+                    to_remove.append(c)
+            for c in to_remove:
+                deck1.remove(c)
+
+            # remove pair from deck2
+            to_remove = []
+            for c in deck2:
+                if c.value == v:
+                    to_remove.append(c)
+            for c in to_remove:
+                deck2.remove(c)
+
+            return self.isBetterHand(deck1, deck2)
+
+        return self.getPairValue(deck1) > self.getPairValue(deck2)
+
     def isBetterHand(self, deck1, deck2):
-        """ return True is deck1 is a better hand than deck2
-        """
-        return self.wichHand(deck1) > self.wichHand(deck2)
+        """ return True is deck1 is a better hand than deck2 """
+        # is there a better hand than an other ?
+        if self.wichHand(deck1) > self.wichHand(deck2):
+            return True
+        if self.wichHand(deck1) < self.wichHand(deck2):
+            return False
+
+        # no clear distinction between the two hands
+        if self.wichHand(deck1) != 0 and\
+                self.wichHand(deck1) == self.wichHand(deck2):
+
+            if self.isQuinteFlush(deck1):
+                if self.searchHighCard(deck1) == self.searchHighCard(deck2):
+                    print 'DRAW'
+                return self.searchHighCard(deck1) > self.searchHighCard(deck2)
+            if self.isSquare(deck1):
+                return self.getSquareValue(deck1) > self.getSquareValue(deck2)
+            if self.isFull(deck1):
+                return self.getThreeValue(deck1) > self.getThreeValue(deck2)
+            if self.isColor(deck1):
+                print 'todo color'
+            if self.isQuinte(deck1):
+                if self.searchHighCard(deck1) == self.searchHighCard(deck2):
+                    print 'DRAW'
+                return self.searchHighCard(deck1) > self.searchHighCard(deck2)
+            if self.isThree(deck1):
+                return self.getThreeValue(deck1) > self.getThreeValue(deck2)
+            if self.isTwoPair(deck1):
+                print 'todo two pairs'
+            if self.isOnePair(deck1):
+                return self.isBetterPair(deck1, deck2)
+
+        # search high card
+        if self.searchHighCard(deck1) > self.searchHighCard(deck2):
+            return True
+        if self.searchHighCard(deck1) < self.searchHighCard(deck2):
+            return False
+        if self.searchHighCard(deck1) == self.searchHighCard(deck2):
+            v = self.searchHighCard(deck1)
+            to_remove = []
+            for c in deck1:
+                if c.value == v:
+                    to_remove.append(c)
+            for e in to_remove:
+                deck1.remove(e)
+
+            to_remove = []
+            for c in deck2:
+                if c.value == v:
+                    to_remove.append(c)
+            for e in to_remove:
+                deck2.remove(e)
+
+            return self.isBetterHand(deck1, deck2)
+
+        print "lol ?!"
 
     def isWorseHand(self, deck1, deck2):
-        """ return True is deck1 is a worse hand than deck2
-        """
-        return self.wichHand(deck1) < self.wichHand(deck2)
+        """ return True is deck1 is a worse hand than deck2 """
+        return not self.isBetterHand(deck1, deck2)
 
     def isSameHand(self, deck1, deck2):
         """ return True is deck1 is the same hand than deck2
             for now a pair is equal to a pair
-            in realty a pair of as is better than a pair of 8
-        """
+            in realty a pair of as is better than a pair of 8 """
         return self.wichHand(deck1) == self.wichHand(deck2)
 
-
+        return self.getThreeValue(deck1) > self.getThreeValue(deck2)
 
 if __name__ == "__main__":
+    f = open('p054_poker.txt')
     i = 0
-    cont = False
-    while (not cont):
-       i+=1
-       game = Poker()
-       deck = game.random_set()
-       cont = game.isCombinaison(deck)
+    for l in f:
+        hands = l.strip()
+
+        hand1 = hands.split(' ')[0:5]
+        hand2 = hands.split(' ')[5:10]
+
+        # load deck 1
+        deck1 = set()
+        for c in hand1:
+            deck1.add(Card(c[0], c[1]))
+
+        # load deck 2
+        deck2 = set()
+        for c in hand2:
+            deck2.add(Card(c[0], c[1]))
+
+        poker = Poker()
+        if poker.isBetterHand(deck1, deck2):
+            i += 1
     print i
